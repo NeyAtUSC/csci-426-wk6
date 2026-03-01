93878b24d011cd816ed2baddc9521958e71481f8
Ney <neycha@usc.edu>
2026-03-01 13:17:21 -0800

    import useful textures

--- prototype6/Assets/Scripts/Player/PlayerMovement.cs
+++ prototype6/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,6 +1,21 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Rigidbody rb;
+
+    public float moveSpeed = 5.0f;
     
+    private Vector2 _moveInput;
+    private void OnMove(InputAction.CallbackContext ctx)
+    {
+        _moveInput = ctx.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 moveDirection = new Vector3(_moveInput.x, 0f, _moveInput.y);
+        rb.AddForce(moveDirection * moveSpeed, ForceMode.Acceleration);
+    }
 }
